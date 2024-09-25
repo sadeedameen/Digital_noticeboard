a5b73a47d83517f3a9af8ace6be93541d363680c
Sadeed Ameen PO <sadeedameen@gmail.com>
2024-09-25 11:53:06 +0530

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -22,6 +22,8 @@
     }
   }
 
+
+                                       
   function _createClass(Constructor, protoProps, staticProps) {
     if (protoProps) _defineProperties(Constructor.prototype, protoProps);
     if (staticProps) _defineProperties(Constructor, staticProps);
